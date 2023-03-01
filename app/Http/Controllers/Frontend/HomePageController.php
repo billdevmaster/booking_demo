8c396df7aa1c6995b8a2b7e393d366ba6ec2b518
billdevmaster71 <billdevmaster@gmail.com>
2023-03-01 19:12:59 +0900

    fix: update time

--- app/Http/Controllers/Frontend/HomePageController.php
+++ app/Http/Controllers/Frontend/HomePageController.php
@@ -287,7 +287,11 @@ class HomePageController extends Controller
         $ret_data['office']['brn_min_time'] = 60 + $location->buffer;
         $ret_data['office']['slot_length'] = $location->interval;
         $day = [];
-        $day['date'] = strtotime($request['start_date']) * 1 - 7200;
+        if ($date1 <= date_create("2023-03-27")) {
+            $day['date'] = strtotime($request['start_date']) * 1 - 7200;
+        } else {
+            $day['date'] = strtotime($request['start_date']) * 1 - 3600;
+        }
         $day['openTimes'] = $this->getLocationOpenTimes($request['office'], $request['start_date']);
         $day['resources'] = $this->getLocationPesuboxs($request['office'], $request['start_date']);
         $ret_data['days'][0] = $day;
