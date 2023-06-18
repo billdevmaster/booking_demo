490217f3fe297c2ba60e2b8782cd995e49167ca5
billdevmaster71 <billdevmaster@gmail.com>
2023-06-18 22:59:53 +0900

    fix: remove footer when homepage

--- resources/views/layouts/backend/app.blade.php
+++ resources/views/layouts/backend/app.blade.php
@@ -153,13 +153,15 @@
 
     <div class="sidenav-overlay"></div>
     <div class="drag-target"></div>
-
-    <!-- BEGIN: Footer-->
-    <footer class="footer footer-static footer-light">
-        <p class="clearfix mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2022<a class="ml-25" href="https://bookid.ee/demo/public" target="_blank"> BOOKid</a><span class="d-none d-sm-inline-block">, All rights Reserved</span></span></p>
-    </footer>
+    @if ($menu != 'home') 
+        <!-- BEGIN: Footer-->
+        <footer class="footer footer-static footer-light">
+            <p class="clearfix mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2022<a class="ml-25" href="https://bookid.ee/demo/public" target="_blank"> BOOKid</a><span class="d-none d-sm-inline-block">, All rights Reserved</span></span></p>
+        </footer>
+    @endif
     <button class="btn btn-primary btn-icon scroll-top" type="button"><i data-feather="arrow-up"></i></button>
     <!-- END: Footer-->
+        
 
     <script>
         var appUrl = "{{ env("APP_URL") }}";
